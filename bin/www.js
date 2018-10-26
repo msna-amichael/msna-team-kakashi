8c5b8fb8af592bb7f94f8dce26dd7c3fafd14df8
Michael Aniemeke <michellin052002@yahoo.com>
2018-10-26 22:30:44 +0200

    database

--- bin/www.js
+++ bin/www.js
@@ -5,4 +5,4 @@ const port = parseInt(process.env.PORT, 10) || 8000;
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port);
+server.listen(port, ()=> console.log('server running'));
