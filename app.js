278aa415b5f6b024639b414e43230a94dffae78d
Michael Aniemeke <michellin052002@yahoo.com>
2018-10-27 17:01:02 +0200

    controller

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
-
 app.use(logger('dev'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
+require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({ message: 'Welcome to MSN Academy.'}));
 
 module.exports = app;
